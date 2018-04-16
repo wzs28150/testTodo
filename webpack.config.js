d35c7e4f4f531f4b67d047a492f7efb16bff33b4
wzs28150 <wz666s@163.com>
2018-04-16 10:15:48 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
+/**
+ * [webpack 配置]
+ * module.exports = {
+    //入口文件的配置项
+    entry:{},
+    //出口文件的配置项
+    output:{},
+    //模块：例如解读CSS,图片如何转换，压缩
+    module:{},
+    //插件，用于生产模版和各项功能
+    plugins:[],
+    //配置webpack开发服务功能
+    devServer:{}
+}
+ */
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
